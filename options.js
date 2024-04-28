319089d838bfa445eecf08cf3b38b5078708141f
davirrocha <davirochamoraes7@gmail.com>
2024-04-28 18:10:39 -0300

    add-functions

--- options.js
+++ options.js
@@ -1,8 +1,11 @@
 const menuOptins = [
-    { name: "Homem Aranha: Aranha-Verso", publishingHouse: "marvel", year: 2020, capa: "./assets/aranhaverso-aranhazero.jpg", codigo: 103, },
-    { name: "Vingadores vs X-Men", publishingHouse: "marvel", year: 2012, capa: "./assets/vingadoresvsxmen.jpg", codigo: 105, },
-    { name: "Amazing Fantasy", publishingHouse: "marvel", year: 2004, capa: "./assets/amazingfanatasy.jpg", codigo: 103, },
-    { name: " Batman A Piada Mortal", publishingHouse: "dc", year: 2016, capa: "./assets/piadamortal.jpg", codigo: 101 },
-
-
+    { name: "Homem Aranha: Aranha-Verso", publishingHouse: "marvel", capa: "./assets/aranhaverso-aranhazero.jpg", codigo: 103, },
+    { name: "Vingadores vs X-Men", publishingHouse: "marvel", capa: "./assets/vingadoresvsxmen.jpg", codigo: 105, },
+    { name: "Amazing Fantasy", publishingHouse: "marvel", capa: "./assets/amazingfanatasy.jpg", codigo: 103, },
+    { name: " Batman A Piada Mortal", publishingHouse: "dc", capa: "./assets/piadamortal.jpg", codigo: 101 },
+    { name: " Flash Point", publishingHouse: "dc", capa: "./assets/flashpoint.jpg", codigo: 102 },
+    { name: " Homem de Ferro: Demônio na Armadura", publishingHouse: "marvel", capa: "./assets/ironman-dna.jpg", codigo: 106 },
+    { name: " Superman Ano Um (2019)", publishingHouse: "dc", capa: "./assets/supermananoum.jpg", codigo: 104 },
 ]
+
+
--- scripts.js
+++ scripts.js
@@ -6,8 +6,7 @@ menuOptins.forEach(item => {
     myList += `
         <li>
             <img src=${item.capa}>
-            <p>${item.name}</p>
-            <p>ANO: ${item.year}</p>
+            <p>${item.name}</p
         </li>
      `
     list.innerHTML = myList
@@ -22,7 +21,6 @@ function allList(comics) {
             <li>
                 <img src=${item.capa}>
                 <p>${item.name}</p>
-                <p>ANO: ${item.year}</p>
             </li>
      `
         list.innerHTML = myList
@@ -47,19 +45,32 @@ function dcList() {
 }
 
 
-const changeComics = document.querySelector(".select-chacter")
-console.log(changeComics)
+const changeComics = document.querySelector(".select-chacter");
 
-const filter = menuOptins.filter(items => items.codigo === 103
+function filter() {
+
+    const batman = menuOptins.filter(item => item.codigo === 101 && changeComics.value === "101")
+    const flash = menuOptins.filter(item => item.codigo === 102 && changeComics.value === "102")
+    const spider = menuOptins.filter(item => item.codigo === 103 && changeComics.value === "103")
+    const xmen = menuOptins.filter(item => item.codigo === 105 && changeComics.value === "105")
+    const iron = menuOptins.filter(item => item.codigo === 106 && changeComics.value === "106")
+    const superman = menuOptins.filter(item => item.codigo === 104 && changeComics.value === "104")
+    allList(batman)
+    allList(flash)
+    allList(spider)
+    allList(xmen)
+    allList(superman)
+    allList(iron)
+}
 
 
-)
-
-console.log(filter)
 
 
 
 
+
+
+changeComics.addEventListener("change", filter)
 const filterMarvel = document.querySelector(".marvel")
 filterMarvel.addEventListener('click', marvelList)
 const filterDc = document.querySelector(".dc")
